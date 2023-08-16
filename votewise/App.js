7f45d6e4b83907fe44c1962ef8c4c291cd2cfbaa
Princess B.  Washington <112123284+princesswG@users.noreply.github.com>
2023-08-16 15:28:21 +0000

    new

--- votewise/App.js
+++ votewise/App.js
@@ -1,24 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './screens/HomeScreen';
+import { View, Text} from 'react-native';
+// import HomeScreen from './screens/HomeScreen';
 
 const Tab = createBottomTabNavigator();
-
+function HomeScreen(){
+  return(
+      <>
+      <View>
+          <Text>this is the home screen</Text>
+      </View>
+      </>
+  )
+}
 export default function App() {
   return (
+   
   <NavigationContainer>
-    <Tab.Screen name='Home' component={HomeScreen}/>
+    <Tab.Screen name='Home' component={HomeScreen} />
   </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   }
+// });
--- votewise/screens/HomeScreen.js
+++ votewise/screens/HomeScreen.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {View, Text} from 'react-native';
 
 
-function HomeScreen({navigation}){
+function HomeScreen(){
     return(
         <>
-        <View style={styles.container}>
+        <View>
             <Text>this is the home screen</Text>
         </View>
         </>
     )
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItem: 'center'
-  }
-})
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItem: 'center'
+//   }
+// })
 export default HomeScreen;
